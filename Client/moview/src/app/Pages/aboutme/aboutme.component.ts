b6f16d4c221e5b3a6b29bbcf6211d29e906b49f8
Ashish Sahu <ashishsahu10428@gmail.com>
2023-06-21 17:29:54 +0530

    profile section updated with data and list of bookings

--- Client/moview/src/app/Pages/aboutme/aboutme.component.ts
+++ Client/moview/src/app/Pages/aboutme/aboutme.component.ts
@@ -1,4 +1,7 @@
 import { Component } from '@angular/core';
+import { Router } from '@angular/router';
+import { HttpClient } from '@angular/common/http';
+import { response } from 'express';
 
 @Component({
   selector: 'app-aboutme',
@@ -7,4 +10,22 @@ import { Component } from '@angular/core';
 })
 export class AboutmeComponent {
 
+  user:any = "";
+
+  constructor(private http: HttpClient) { }
+  ngOnInit(){
+    const url = `http://localhost:5000/api/user/${localStorage.getItem('loggeduserdetails')}`
+    this.http.get(url).subscribe(
+      {
+        next:response=>{
+          console.log(response);
+          this.user = response;
+        },
+        error:error=>{
+          console.log(error);
+        }
+      }
+    )
+    console.log(this.user);
+  }
 }
