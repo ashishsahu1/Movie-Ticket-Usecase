d9a0094468f3ebde914336d7845a893c9e411e03
Ashish Sahu <ashishsahu10428@gmail.com>
2023-06-25 19:06:26 +0530

    singup added

--- Client/moview/src/app/Components/signup/signup.component.ts
+++ Client/moview/src/app/Components/signup/signup.component.ts
@@ -1,4 +1,6 @@
 import { Component } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-signup',
@@ -6,5 +8,25 @@ import { Component } from '@angular/core';
   styleUrls: ['./signup.component.css']
 })
 export class SignupComponent {
+  formData = {
+    email: '',
+    name: '',
+    password: ''
+  };
 
+  constructor(private http: HttpClient, private router:Router) { }
+
+  onSubmit(){
+    console.log(this.formData);
+    this.http.post<any>('http://localhost:5000/api/user/signup', this.formData).subscribe({
+      next:response=>{
+        console.log(response);
+        alert("Signup Successful")
+      },
+      error:error=>{
+        console.log(error);
+      }
+    })
+    this.router.navigate(['/auth'])
+  }
 }
